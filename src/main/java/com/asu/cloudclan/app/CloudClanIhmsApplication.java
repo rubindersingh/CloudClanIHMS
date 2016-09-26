15c327a275ad41bce82145463003b82ac6225fe4
Rubinder Singh <rssandhu2011@gmail.com>
2016-09-26 15:13:12 -0700

    Spring boot app basic structure setup

---
+++ src/main/java/com/asu/cloudclan/app/CloudClanIhmsApplication.java
@@ -0,0 +1,15 @@
+package com.asu.cloudclan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.asu.cloudclan")
+public class CloudClanIhmsApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(CloudClanIhmsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/asu/cloudclan/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.asu.cloudclan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam String name) {
+		return "Hello "+name;
+	}
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
